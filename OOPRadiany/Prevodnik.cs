0a755a73018e44e9a76b8a7ab6e2297c4f11f5f2
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-11 12:33:21 +0100

    Add project files.

---
+++ OOPRadiany/Prevodnik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPRadiany
+{
+    static class  Prevodnik
+    {
+        
+        public static string PrevedNaRadiany(double degreeInput)
+        {
+            double radians = Math.Round((Math.PI / 180) * degreeInput, 13);
+           
+            return String.Format("{0} stupňů na radiány: {1}", degreeInput, radians);
+        }
+
+        public static string PrevedNaStupne(double radianInput)
+        {
+            double degrees = (180 / Math.PI) * radianInput;
+
+            return String.Format("{0} radiánů na stupně: {1}", radianInput, degrees);
+        }
+    }
+}
---
+++ OOPRadiany/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOPRadiany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Prevodnik.PrevedNaStupne(6.28));
+            Console.WriteLine(Prevodnik.PrevedNaRadiany(90));
+            Console.ReadKey();
+        }
+    }
+}
